da9fcd3be0fc05aac22f9a247774e2f3053b69fc
Jeronimo Cardu <jeronimocardu29@gmail.com>
2025-04-17 15:05:13 -0300

    README update

--- src/routes/error/ErrorDetail.tsx
+++ src/routes/error/ErrorDetail.tsx
@@ -4,11 +4,13 @@ function ErrorDetail() {
   const error = useRouteError() as { status?: number };
   return (
     <>
-      <main className="flex h-full w-fit flex-col !mx-auto items-center justify-center text-center">
+      <main className="!mx-auto flex h-full w-fit flex-col items-center justify-center text-center">
         <h1 className="textPreset1">
           We can't seem to find the page you're looking for.
         </h1>
-        <p className="textPreset4 text-neutral-600 dark:text-neutral-400">Error code: {error.status}</p>
+        <p className="textPreset4 text-neutral-600 dark:text-neutral-400">
+          Error code: {error.status}
+        </p>
         <NavLink
           to={"/"}
           className="!my-6 !underline !decoration-blue-500 underline-offset-3 dark:!decoration-blue-700"
