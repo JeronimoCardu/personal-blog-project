d495da877783f13077f00a5aa3e8431cdb5e8f9d
Jeronimo Cardu <jeronimocardu29@gmail.com>
2025-04-17 14:57:51 -0300

    feat: add error detail

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,11 +5,13 @@ import Blog from "./Blog";
 import Home from "./Home";
 import NavBar from "../components/NavBar";
 import SingleBlog from "./Blog/SingleBlog";
+import ErrorDetail from "./error/ErrorDetail";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <NavBar />,
+    errorElement: <ErrorDetail/>,
     children: [
       { path: "/", element: <Home /> },
       { path: "/blog", element: <Blog /> },
