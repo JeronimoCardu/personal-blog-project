104f30f99aea9e019011d12ea721a89f1ab2f6bc
Jeronimo Cardu <jeronimocardu29@gmail.com>
2025-04-17 12:51:33 -0300

    fix: src change | media img with imports

--- src/components/Media.tsx
+++ src/components/Media.tsx
@@ -1,8 +1,8 @@
 import { Link } from "react-router-dom";
-import imgX from "src/assets/images/logo-x.svg";
-import imgGitHub from "src/assets/images/logo-github.svg";
-import imgLinkedIn from "src/assets/images/logo-linkedin.svg";
-import imgFrontendMentor from "src/assets/images/logo-frontend-mentor.svg";
+import imgX from "../assets/images/logo-x.svg";
+import imgGitHub from "../assets/images/logo-github.svg";
+import imgLinkedIn from "../assets/images/logo-linkedin.svg";
+import imgFrontendMentor from "../assets/images/logo-frontend-mentor.svg";
 
 type Props = {
   part?: string;
@@ -15,31 +15,19 @@ function Media({ part }: Props) {
         to="#"
         className={`${part == "footer" ? "!border-none !bg-transparent" : "!border-nautral-200 flex h-[2.5em] w-[2.5em] items-center justify-center rounded-[0.625em] !border-1 bg-white p-2 hover:border-neutral-300 hover:bg-neutral-200 dark:border-neutral-700 dark:bg-neutral-800 dark:!text-white dark:hover:border-neutral-600 dark:hover:bg-neutral-700"}`}
       >
-        <img
-          className="dark:invert"
-          src={imgX}
-          alt="X"
-        />
+        <img className="dark:invert" src={imgX} alt="X" />
       </Link>
       <Link
         to="#"
         className={`${part == "footer" ? "!border-none !bg-transparent" : "!border-nautral-200 flex h-[2.5em] w-[2.5em] items-center justify-center rounded-[0.625em] !border-1 bg-white p-2 hover:border-neutral-300 hover:bg-neutral-200 dark:border-neutral-700 dark:bg-neutral-800 dark:!text-white dark:hover:border-neutral-600 dark:hover:bg-neutral-700"}`}
       >
-        <img
-          className="dark:invert"
-          src={imgGitHub}
-          alt="GitHub"
-        />
+        <img className="dark:invert" src={imgGitHub} alt="GitHub" />
       </Link>
       <Link
         to="#"
         className={`${part == "footer" ? "!border-none !bg-transparent" : "!border-nautral-200 flex h-[2.5em] w-[2.5em] items-center justify-center rounded-[0.625em] !border-1 bg-white p-2 hover:border-neutral-300 hover:bg-neutral-200 dark:border-neutral-700 dark:bg-neutral-800 dark:!text-white dark:hover:border-neutral-600 dark:hover:bg-neutral-700"}`}
       >
-        <img
-          className="dark:invert"
-          src={imgLinkedIn}
-          alt="LinkedIn"
-        />
+        <img className="dark:invert" src={imgLinkedIn} alt="LinkedIn" />
       </Link>
       <Link
         to="#"
